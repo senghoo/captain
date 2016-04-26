45ef76fafcafb724f3b64cb7d223a9ca243281fb
Senghoo Kim <shkdmb@gmail.com>
2016-04-26 16:50:53 +0800

    add middleware

--- web/server.go
+++ web/server.go
@@ -1,6 +1,11 @@
 package web
 
-import "gopkg.in/macaron.v1"
+import (
+	"github.com/go-macaron/cache"
+	"github.com/go-macaron/pongo2"
+	"github.com/go-macaron/session"
+	"gopkg.in/macaron.v1"
+)
 
 type Server struct {
 	m *macaron.Macaron
@@ -14,5 +19,16 @@ func NewServer() *Server {
 
 func (s *Server) Run() {
 	s.initRoute()
+	s.m.SetAutoHead(true)
 	s.m.Run()
+}
+
+func (s *Server) initMiddleWare() {
+	s.m.Use(macaron.Logger())
+	s.m.Use(macaron.Recovery())
+	s.m.Use(macaron.Static("public"))
+	s.m.Use(macaron.Static("assets"))
+	s.m.Use(cache.Cacher())
+	s.m.Use(session.Sessioner())
+	s.m.Use(pongo2.Pongoer())
 }
