82b3c73eb3bff2a29e9c4f713b13e3598ca0165a
Senghoo Kim <shkdmb@gmail.com>
2016-04-30 08:39:05 +0800

    add sign_in form

--- web/controllers/user/sign.go
+++ web/controllers/user/sign.go
@@ -5,3 +5,13 @@ import "gopkg.in/macaron.v1"
 func SignIn(ctx *macaron.Context) {
 	ctx.HTML(200, "user/sign_in")
 }
+
+func SignInPost(ctx *macaron.Context, form SignInForm) {
+	ctx.HTML(200, "user/sign_in")
+}
+
+type SignInForm struct {
+	UserName string `binding:"Required;MaxSize(254)"`
+	Password string `binding:"Required;MaxSize(254)"`
+	Remember bool
+}
--- web/route.go
+++ web/route.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"github.com/go-macaron/binding"
 	"github.com/senghoo/captain/web/controllers"
 	"github.com/senghoo/captain/web/controllers/user"
 )
@@ -9,5 +10,6 @@ func (s *Server) initRoute() {
 	s.m.Get("/", controllers.Main)
 	s.m.Group("/user", func() {
 		s.m.Get("/sign_in", user.SignIn)
+		s.m.Post("/sign_in", binding.Bind(user.SignInForm{}), user.SignIn)
 	})
 }
