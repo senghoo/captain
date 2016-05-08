45d10bf859354c2610223421c41e8c72e03c02e5
Senghoo Kim <shkdmb@gmail.com>
2016-05-08 17:57:08 +0800

    add docker models

---
+++ models/docker.go
@@ -0,0 +1,37 @@
+package models
+
+import "time"
+
+type DockerServer struct {
+	ID       int64
+	Name     string `xorm:"not null unique"`
+	Endpoint string
+	Created  time.Time `xorm:"CREATED"`
+	Updated  time.Time `xorm:"UPDATED"`
+	Deleted  time.Time `xorm:"deleted"`
+}
+
+func NewDockerServer(name, endpoint string) *DockerServer {
+	return &DockerServer{
+		Name:     name,
+		Endpoint: endpoint,
+	}
+}
+
+func (d *DockerServer) Save() {
+	if d.ID == 0 {
+		// find same user
+		cond := &DockerServer{
+			Name: d.Name,
+		}
+		has, _ := x.Get(cond)
+		if has {
+			// got it
+			x.Id(cond.ID).Update(d)
+			return
+		}
+		x.Insert(d)
+	} else {
+		x.Id(d.ID).Update(d)
+	}
+}
