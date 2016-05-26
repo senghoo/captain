b22fdc4deba8ef3e32c4b93578116d6ed89d3f53
Senghoo Kim <shkdmb@gmail.com>
2016-05-26 17:27:34 +0800

    add repo path

--- models/repo.go
+++ models/repo.go
@@ -1,9 +1,16 @@
 package models
 
 import (
+	"errors"
 	"fmt"
+	"path"
 	"strings"
 	"time"
+)
+
+const (
+	REPOSITORY_STATUS_IDLE = iota
+	REPOSITORY_STATUS_CLONEING
 )
 
 type Repository struct {
@@ -25,6 +32,7 @@ type Repository struct {
 	GitURL        string
 	SSHURL        string
 	Language      string
+	Status        int
 	Created       time.Time `xorm:"CREATED"`
 	Updated       time.Time `xorm:"UPDATED"`
 	Deleted       time.Time `xorm:"deleted"`
@@ -49,4 +57,43 @@ func GetRepositoryByIdentify(identify string) (*Repository, error) {
 
 func (r *Repository) Identify() string {
 	return fmt.Sprintf("%s:%s", r.Site, r.FullName)
+}
+
+func (r *Repository) Clone() {
+	if r.Status == REPOSITORY_STATUS_IDLE {
+		go r.clone()
+	}
+}
+
+func (r *Repository) clone() {
+	r.Status = REPOSITORY_STATUS_CLONEING
+	x.Id(r.ID).Update(r)
+	defer func() {
+		r.Status = REPOSITORY_STATUS_IDLE
+		x.Id(r.ID).Update(r)
+	}()
+
+}
+
+func (r *Repository) Workspace() *Workspace {
+	if r.workspace != nil {
+		return r.workspace
+	}
+
+	if r.WorkspaceID != 0 {
+		ws := new(Workspace)
+		has, _ := GetByID(r.WorkspaceID, ws)
+		if has {
+			return ws
+		}
+	}
+	return nil
+}
+
+func (r *Repository) Path() (string, error) {
+	ws := r.Workspace()
+	if ws == nil {
+		return "", errors.New("Workspace not exists")
+	}
+	return path.Join(ws.WorkDir(), "repos", r.FullName)
 }
