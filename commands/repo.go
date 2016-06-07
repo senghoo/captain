073a0c3f482523eb63c7ca13191262698d434cbb
Senghoo Kim <shkdmb@gmail.com>
2016-06-07 19:42:25 +0800

    repo archive command

--- commands/repo.go
+++ commands/repo.go
@@ -48,3 +48,44 @@ func (r *RepoUpdateCommand) Next() Command {
 func (r *RepoUpdateCommand) SetNext(c Command) {
 	r.next = c
 }
+
+type RepoArchiveCommand struct {
+	RepoID int64
+	Status int
+	next   Command
+}
+
+func NewRepoArchiveCommand(repoID int64) *RepoUpdateCommand {
+	return &RepoUpdateCommand{
+		RepoID: repoID,
+	}
+}
+
+func (r *RepoArchiveCommand) Run(build *models.Build) {
+	logger := build.Logger()
+
+	repo := new(models.Repository)
+	has, err := models.GetByID(r.RepoID, repo)
+	if !has {
+		logger.Printf("Error: %d has not exists", r.RepoID)
+		return
+	}
+	if err != nil {
+		logger.Printf("Error: %s", err)
+		return
+	}
+	out, err := repo.Archive()
+	logger.Printf("Output:\n>>>>>%s\n<<<<<", out)
+	if err != nil {
+		logger.Printf("Error: %s", err)
+		return
+	}
+}
+
+func (r *RepoArchiveCommand) Next() Command {
+	return r.next
+}
+
+func (r *RepoArchiveCommand) SetNext(c Command) {
+	r.next = c
+}
