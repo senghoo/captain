7d1242eb2c38eafe36f1be9e4853f46989f37a7f
Senghoo Kim <shkdmb@gmail.com>
2016-06-25 19:38:58 +0800

    new command

--- commands/command.go
+++ commands/command.go
@@ -1,14 +1,62 @@
 package command
 
-import "github.com/senghoo/captain/models"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/senghoo/captain/models"
+)
+
+type CommandArgs map[string]interface{}
 
 type Command interface {
-	Run(build *models.Build)
-	Next() Command
+	Run(build *models.Build) string
+	Clone(args CommandArgs) (Command, error)
 }
 
-func RunCommand(cmd Command, build *models.Build) {
-	for ; cmd != nil; cmd = cmd.Next() {
-		cmd.Run(build)
+var commandMap map[string]Command
+
+func RegisterCommand(name string, c Command) {
+	commandMap[name] = c
+}
+
+func NewCommand(name string, args CommandArgs) (Command, error) {
+	c, ok := commandMap[name]
+	if !ok {
+		return nil, fmt.Errorf("Command %s not found", name)
 	}
+	return c.Clone(args)
+}
+
+type Node struct {
+	CommandName string
+	CommandArgs CommandArgs
+	SubNode     map[string]Node
+}
+
+func (n *Node) Command() (Command, error) {
+	return NewCommand(n.CommandName, n.CommandArgs)
+}
+
+func ParseNode(raw []byte) (n *Node, err error) {
+	n = new(Node)
+	err = json.Unmarshal(raw, n)
+	return
+}
+
+func RunNode(n *Node, build *models.Build) {
+	logger := build.Logger()
+	command, err := n.Command()
+	if err != nil {
+		logger.Printf("create command error %s", err)
+	}
+
+	status := command.Run(build)
+
+	subnode, ok := n.SubNode[status]
+	if !ok {
+		return
+	}
+
+	RunNode(subnode, build)
 }
